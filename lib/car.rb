774a3727494eb1fe3b765e98d19999abfc4e1f97
Firas Chaabani <firaschaabani1@gmail.com>
2020-10-14 02:20:21 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -4,8 +4,9 @@ class Car
   @all = []
   def initialize (car)
       @@vehicle = vehicle
-        @all << vehicle
-  @all << vehicle
+      @@all << vehicle
+  end
+  
     def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
