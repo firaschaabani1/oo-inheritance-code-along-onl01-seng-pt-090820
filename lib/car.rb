fd85fd3816baf6c2e08e42e2287f0b7fd3b74446
Firas Chaabani <firaschaabani1@gmail.com>
2020-10-14 02:22:07 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,10 +2,8 @@ require_relative "./vehicle.rb"
 class Car 
   
   @all = []
-  def initialize (car)
-      @@vehicle = vehicle
-    
-  end
+  
+  
   
     def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
